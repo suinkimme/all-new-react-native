b638c6112572b05da00064ba39a1e915df22907a
Suin Kim <contact@suinkim.me>
2024-07-11 17:39:22 +0900

    vsc github actions 테스트

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { VERSION } from "./env";
 
 export default function App() {
-  console.log(VERSION);
-
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
